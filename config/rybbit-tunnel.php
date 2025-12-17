c7f6855752ccda6f05f2acda01074e3acce2222e
Carlo Eusebi <carloeusebi@gmail.com>
2025-12-17 10:54:29 +0100

    feat: added configurable middlewares

--- config/rybbit-tunnel.php
+++ config/rybbit-tunnel.php
@@ -10,4 +10,8 @@ return [
     // Enable verbose logging for debugging purposes. When true, incoming request
     // headers will be logged by the proxy controller.
     'debug' => env('RYBBIT_TUNNEL_DEBUG', false),
+
+    'middleware' => [
+        'web',
+    ],
 ];
--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,15 @@ use Carloeusebi\RybbitTunnel\Http\Controllers\RybbitProxyController;
 use Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix(config('rybbit-tunnel.tunnel-url'))->group(function () {
-    Route::get('/{script}', [RybbitProxyController::class, 'proxyScript'])->where('script', '(script|script-full|replay|metrics)\.js');
+Route::prefix(config('rybbit-tunnel.tunnel-url'))
+    ->middleware(config('rybbit-tunnel.middleware'))
+    ->withoutMiddleware(ConvertEmptyStringsToNull::class)
+    ->group(function () {
+        Route::get('/{script}', [RybbitProxyController::class, 'proxyScript'])->where('script', '(script|script-full|replay|metrics)\.js');
 
-    Route::post('/track', [RybbitProxyController::class, 'proxyTrack']);
-    Route::post('/identify', [RybbitProxyController::class, 'proxyIdentify']);
-    Route::post('/session-replay/record/{siteId}', [RybbitProxyController::class, 'proxySessionReplay']);
+        Route::post('/track', [RybbitProxyController::class, 'proxyTrack']);
+        Route::post('/identify', [RybbitProxyController::class, 'proxyIdentify']);
+        Route::post('/session-replay/record/{siteId}', [RybbitProxyController::class, 'proxySessionReplay']);
 
-    Route::get('/site/tracking-config/{siteId}', [RybbitProxyController::class, 'proxySiteConfig']);
-})->withoutMiddleware(ConvertEmptyStringsToNull::class);
+        Route::get('/site/tracking-config/{siteId}', [RybbitProxyController::class, 'proxySiteConfig']);
+    });
